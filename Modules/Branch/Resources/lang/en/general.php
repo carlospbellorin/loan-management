e301bdf8d844613c952888abf7976fcccf639e6d
kkobtk@gmail.com <K4m1s4m4$.Lucas>
2022-07-14 19:08:11 -0400

    Initial configuration

---
+++ Modules/Branch/Resources/lang/en/general.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tj
+ * Date: 7/6/19
+ * Time: 12:20 PM
+ */
+return [
+    'user_already_added_to_branch' => 'Usuario ya agregado a sucursal',
+    'open'=>'Abierto',
+    'branch'=>'Sucursal|Sucursales',
+];
---
+++ Modules/Dashboard/Resources/lang/en/general.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tj
+ * Date: 8/2/19
+ * Time: 7:55 PM
+ */
+return [
+    'dashboard' => 'Tablero|Tableros',
+    'widget' => 'Widget|Widgets',
+    'successfully_rearranged'=>'Tablero reorganizado con éxito',
+    'failed_rearrange'=>'Error al reorganizar el tablero',
+    'drag_widget_here_to_delete'=>'Arrastre el widget aquí para eliminarlo',
+];
